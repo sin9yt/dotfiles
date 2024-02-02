52b6e647c8921dc7f78d57e9f4cfe4fd1a770daa
Sindhoor Tilak <sindhoor@sin9yt.net>
2024-02-02 05:38:07 +0000

    nvim: treesitter initial config

--- nvim/lua/sin9yt/lazy/treesitter.lua
+++ nvim/lua/sin9yt/lazy/treesitter.lua
@@ -1,1 +1,49 @@
-return {}
+return {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter-textobjects",
+    },
+
+    config = function()
+        -- import nvim-treesitter plugin
+        local treesitter = require("nvim-treesitter.configs")
+
+        -- configure treesitter
+        treesitter.setup({ -- enable syntax highlighting
+            highlight = {
+                enable = false,
+            },
+            -- enable indentation
+            indent = { enable = true },
+            -- ensure these language parsers are installed
+            ensure_installed = {
+                "json",
+                "javascript",
+                "python",
+                "rust",
+                "tsx",
+                "yaml",
+                "html",
+                "markdown",
+                "markdown_inline",
+                "graphql",
+                "bash",
+                "lua",
+                "vim",
+                "dockerfile",
+                "gitignore",
+            },
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                    init_selection = "<C-space>",
+                    node_incremental = "<C-space>",
+                    scope_incremental = false,
+                    node_decremental = "<bs>",
+                },
+            },
+        })
+
+    end
+}
