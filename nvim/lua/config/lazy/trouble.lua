3e1b552a4e996138101e66e16892d48426e3e29b
Sindhoor Tilak <sindhoorhegde378@gmail.com>
2025-07-05 05:22:39 +0000

    nvim: formatting of trouble

--- nvim/lua/config/lazy/trouble.lua
+++ nvim/lua/config/lazy/trouble.lua
@@ -1,25 +1,25 @@
 return {
 	"folke/trouble.nvim",
 	config = function()
-		require("trouble").setup({
-			opts = {},
-			cmd = "Trouble",
+        require("trouble").setup({
+            opts = {},
+            cmd = "Trouble",
             focus = true,
-			auto_close = true,
-		})
+            auto_close = true,
+        })
 
-		local trouble = require("trouble")
+        local trouble = require("trouble")
 
-		vim.keymap.set("n", "<leader>tt", function()
-			trouble.toggle("diagnostics")
-		end)
+        vim.keymap.set("n", "<leader>tt", function()
+            trouble.toggle("diagnostics")
+        end)
 
-		vim.keymap.set("n", "]d", function()
-			trouble.next("diagnostics")
-		end)
+        vim.keymap.set("n", "]d", function()
+            trouble.next("diagnostics")
+        end)
 
-		vim.keymap.set("n", "[d", function()
-			trouble.prev("diagnostics")
-		end)
-	end,
+        vim.keymap.set("n", "[d", function()
+            trouble.prev("diagnostics")
+        end)
+    end,
 }
